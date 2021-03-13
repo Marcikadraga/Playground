982511aa1fa2f7c0979c11350fce8ed7ce939c30
Marci <marciprog@gmail.com>
2021-03-13 21:52:01 +0100

    Dino is running

--- GoogleTRex/TRex.js
+++ GoogleTRex/TRex.js
@@ -22,7 +22,7 @@ class Field {
         field.style.position = "absolute";
         field.style.backgroundColor = "rgb(255,255,255)";
         document.getElementById("drawingTable").appendChild(field);
-        // field.onclick = function () { this.style.backgroundColor = document.getElementById("statusCanvas1").style.backgroundColor }
+
     }
 }
 class Table {
@@ -36,7 +36,7 @@ class Table {
     DisplayTheTable() {
 
         var id = 0;
-        var fieldsize = 15
+        var fieldsize = 3
         for (var i = 0; i < this.yTableSize; i++) {
             this.array.push([]);
             for (var j = 0; j < this.xTableSize; j++) {
@@ -44,6 +44,7 @@ class Table {
                 id++
             }
         }
+        // console.log(this.array);
         var newArray = [];
         var counter = 0;
         for (var i = 0; i < this.yTableSize; i++) {
@@ -55,38 +56,75 @@ class Table {
                 if (i % 2 == 0 && j % 2 == 0 || i % 2 != 0 && j % 2 != 0) {
                     document.getElementById(newArray[i][j]).style.background = "rgb(216, 212, 212)";
                 }
+
+
             }
         }
-        //set the drawTable's border's size
+
         document.getElementById("drawingTable").style.height = this.yTableSize * fieldsize + "px";
         document.getElementById("drawingTable").style.width = this.xTableSize * fieldsize + "px";
-        var ctx = document.createElement('canvas').getContext('2d');
-        var color;
-        var colorArray = [];
-
-        for (var i = 0; i < this.yTableSize; i++) {
-            colorArray.push([]);
-            for (var j = 0; j < this.xTableSize; j++) {
-                // //https://stackoverflow.com/questions/5999209/how-to-get-the-background-color-code-of-an-element-in-hex
-                color = document.getElementById(newArray[i][j]).style.backgroundColor;
-                ctx.strokeStyle = color;
-                var hexColor = ctx.strokeStyle;
-                colorArray[i].push(hexColor);
+    
+    
+        $.getJSON("Data.json", function (json) {
+           var counter = 0;
+            const timer = ms => new Promise(res => setTimeout(res, ms))
+            async function load() {
+                while (true) {
+                    await timer(100);
+    
+    
+                    if (counter % 2 == 0) {
+                        var dino = json[4].Data;
+                        for (var i = 0; i < dino.length; i++) {
+                            for (var j = 0; j < dino[0].length; j++) {
+                                document.getElementById(newArray[i+130-dino.length][j+6]).style.backgroundColor = dino[i][j];
+    
+                            }
+                        }
+                    }
+    
+                    else {
+                        var dino = json[3].Data;
+                        for (var i = 0; i < dino.length; i++) {
+                            for (var j = 0; j < dino[0].length; j++) {
+                                document.getElementById(newArray[i+130-dino.length][j+6]).style.backgroundColor = dino[i][j];
+                            }
+                        }
+                    }
+                    counter++;
+                }
             }
-        }
+            load();
+        });
+    
     }
     CreateNewTable() {
-        
-            var canvasesLength = document.getElementById("drawingTable").getElementsByTagName("canvas").length;
-            document.getElementById("drawingTable").style.visibility = "visible";
-            for (var i = 0; i < canvasesLength; i++) {
-                document.getElementById(i).remove();
-            }
 
-            
-            t.DisplayTheTable();
+        var canvasesLength = document.getElementById("drawingTable").getElementsByTagName("canvas").length;
+        document.getElementById("drawingTable").style.visibility = "visible";
+        for (var i = 0; i < canvasesLength; i++) {
+            document.getElementById(i).remove();
+        }
+
+
+
+
+    }
+}
+class Main {
+    StartTheProgram() {
+        var t = new Table(130, 500);
+        t.CreateNewTable();
+        t.DisplayTheTable();
         
     }
 }
-var t = new Table();
-t.CreateNewTable();
+var m = new Main();
+m.StartTheProgram();
+
+
+
+
+
+
+
